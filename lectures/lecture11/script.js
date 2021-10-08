4017f3e975f67ec697656ee82d61dc5ae049a907
Vlad Balabash <vladb951@gmail.com>
2021-10-09 00:32:59 +0300

    lesson 12

--- lectures/lecture11/script.js
+++ lectures/lecture11/script.js
@@ -1,6 +1,10 @@
 console.log(document);
-console.dir(first);
-first.autocapitalize = 'words';
+console.dir(document.body.children[0]);
+
+// don't use global vars
+// console.dir(first);
+// first.autocapitalize = 'words';
+
 
 // !!!!DON'T DO THAT!!!!
 // document
@@ -38,3 +42,43 @@ first.autocapitalize = 'words';
 // for (let i = 0; i < els.length; i++) {
 //     els[i].innerText = els[i].innerText + ' with class secondary';
 // }
+
+const first = document.getElementById('first');
+
+// classList
+
+// adds class to Element
+first.classList.add('class-added-by-js');
+
+// removes class from Element
+first.classList.remove('first-styles');
+
+// check if class is presented
+console.log(first.classList.contains('class-added-by-js'));
+console.log(first.classList.contains('first-styles'));
+
+// replaces class with another one
+first.classList.replace('class-added-by-js', 'new-class');
+
+// adds class if it is presented and removes in opposite case
+first.classList.toggle('new-class');
+
+console.log(first.classList.value);
+
+console.dir(first);
+
+console.log(first.dataset.id);
+
+const main = document.getElementsByTagName('main')[0];
+console.dir(main);
+
+function onButtonClick() {
+    console.log('btn was clicked!');
+}
+
+const btnEl = document.getElementById('demoBtn');
+btnEl.onclick = onButtonClick;
+
+
+
+
